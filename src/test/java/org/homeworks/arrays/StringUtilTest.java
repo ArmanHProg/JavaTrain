cc351f13cbc6376f6e866aa08ade729f8bbee598
Arman Hakhverdyan <arman.hakhverdyan@bk.ru>
2023-03-21 22:09:43 +0400

    a

--- src/test/java/org/homeworks/arrays/StringUtilTest.java
+++ src/test/java/org/homeworks/arrays/StringUtilTest.java
@@ -42,7 +42,7 @@ public class StringUtilTest {
        StringUtil stringUtil = new StringUtil();
        String actual = stringUtil.longestWord();
        Assertions.assertEquals(expected,actual);
-
+       System.out.println("gugo");
    }
 
    @Test
